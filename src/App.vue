80ad1e19c5511341d9702f3d84993ecb748416d6
林俏耿 <qiaogeng.lin@jimicn.com>
2018-11-06 15:40:07 +0800

    增加动画

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <el-button @click="show =!show">fade淡入淡出</el-button>
+      <div style="display:flex;margin-top:20px;height:100px;">
+        <transition name="el-fade-in-linear">
+          <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+        </transition>
+        <transition name="el-fade-in">
+          <div v-show="show" class="transition-box">.el-fade-in</div>
+        </transition>
+      </div>
+
+      <el-button @click="showSecond=!showSecond" style="margin-top:20px">zoom缩放</el-button>
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-zoom-in-center">
+        <div v-show="showSecond" class="transition-box">.el-zoom-in-center</div>
+      </transition>
+
+      <transition name="el-zoom-in-top">
+        <div v-show="showSecond" class="transition-box">.el-zoom-in-top</div>
+      </transition>
+
+      <transition name="el-zoom-in-bottom">
+        <div v-show="showSecond" class="transition-box">.el-zoom-in-bottom</div>
+      </transition>
+    </div>
+  
+
+   <div>
+    <el-button @click="showThird = !showThird" style="margin-top:20px">collapse展开折叠</el-button>
+
+    <div style="margin-top: 20px; height: 200px;">
+      <el-collapse-transition>
+        <div v-show="showThird">
+          <div class="transition-box">el-collapse-transition</div>
+          <div class="transition-box">el-collapse-transition</div>
+        </div>
+      </el-collapse-transition>
+    </div>
+   </div>
+
+  </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data: function() {
+    return {
+      show: true,
+      showSecond: true,
+      showThird: true
+    };
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.transition-box {
+  margin-bottom: 10px;
+  width: 200px;
+  height: 100px;
+  border-radius: 4px;
+  background-color: #409eff;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #fff;
+  padding: 40px 20px;
+  box-sizing: border-box;
+  /*  border-box 为元素指定的任何内边距和边框都将在已设定的宽度和高度内进行绘制 */
+  margin-right: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
+import Vue from 'vue';
 import App from './App.vue'
-
+import './plugins/element.js'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+
+import { Button, Select } from 'element-ui';
+
+Vue.component(Button.name, Button);
+Vue.component(Select.name, Select);
+
+Vue.use(ElementUI);
 
 new Vue({
   render: h => h(App),
